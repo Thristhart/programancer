5aad79b7f08d05930fa06b5b1d22216bbe219237
Tom Shea <tom@shea.at>
2013-10-09 20:19:33 +0000

    Headache-inducing scope error caused by simple missing 'var'...

--- scripts/ents/background_element.js
+++ scripts/ents/background_element.js
@@ -3,6 +3,6 @@ BackgroundElement = Entity.child();
 // Ensure that the element doesn't subscribe to the update loop, and instead subscribes to
 // the background update call
 BackgroundElement.prototype.registerListener = function() {
-  me = this;
+  var me = this;
   canvas.addEventListener("renderBackground", function() {me.draw(backgroundContext)});
 }
