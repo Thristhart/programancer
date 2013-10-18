85636fd435acb327c37ee5b56dbf1f13c5330fdf
Tom Shea <tom@shea.at>
2013-10-18 00:36:47 +0000

    Implemented mouse input events

--- scripts/input/mouse.js
+++ scripts/input/mouse.js
@@ -9,10 +9,73 @@ canvas.addEventListener("mousemove", function(event) {
         interactible_entities[i].y < y &&
         interactible_entities[i].x + interactible_entities[i].width > x &&
         interactible_entities[i].y + interactible_entities[i].height > y)
-      doHover(interactible_entities[i], x, y);
+      return doHover(interactible_entities[i], x, y);
+  }
+  // if we got here there is no hover target
+  currentHoverTarget = null;
+});
+
+canvas.addEventListener("mousedown", function(event) {
+  var x = event.pageX - canvas.offsetLeft;
+  var y = event.pageY - canvas.offsetTop;
+  if(currentHoverTarget) {
+    doClickDown(currentHoverTarget, x, y);
+  }
+});
+canvas.addEventListener("mouseup", function(event) {
+  var x = event.pageX - canvas.offsetLeft;
+  var y = event.pageY - canvas.offsetTop;
+  if(currentClickDownTarget) {
+    doClick(currentClickDownTarget, x, y);
   }
 });
 
+var currentHoverTarget;
 function doHover(target, mouseX, mouseY) {
-  console.log(target.x, target.y);
+  var hoverEvent = new CustomEvent("hover", {
+    detail: {
+      x: mouseX,
+      y: mouseY,
+      target: target
+    },
+    bubbles: true,
+    cancelable: false
+  });
+
+  currentHoverTarget = target;
+
+  mouseHandler.dispatchEvent(hoverEvent);
 }
+
+var currentClickDownTarget;
+function doClickDown(target, mouseX, mouseY) {
+  currentClickDownTarget = target;
+
+  var clickDownEvent = new CustomEvent("clickDown", {
+    detail: {
+      x: mouseX,
+      y: mouseY,
+      target: target
+    },
+    bubbles: true,
+  });
+
+  mouseHandler.dispatchEvent(clickDownEvent);
+}
+
+function doClick(target, mouseX, mouseY) {
+  var clickEvent = new CustomEvent("click", {
+    detail: {
+      x: mouseX,
+      y: mouseY,
+      target: target
+    },
+    bubbles: true,
+  });
+  
+  currentClickDownTarget = null; // once we've clicked, we should no longer have a target
+  
+  mouseHandler.dispatchEvent(clickEvent);
+}
+
+var mouseHandler = document.createElement("div");
