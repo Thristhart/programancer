9569922c54431af416484f0c1e8c01d6d8371818
Tom Shea <tom@shea.at>
2013-10-19 10:07:50 +0000

    Players draw blue

--- scripts/ents/player.js
+++ scripts/ents/player.js
@@ -6,6 +6,11 @@ Player.prototype.constructors.push(function() {
   cameraFocus = this; // make the camera follow us
 });
 
+Player.prototype.draw = function() {
+  context.fillStyle = "blue";
+  context.fillRect(this.x, this.y, this.width, this.height);
+}
+
 function createPlayerAt(x, y) {
   player = new Player(WALL_WIDTH * x, WALL_HEIGHT * y);
 }
