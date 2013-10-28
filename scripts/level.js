ffccb85fac9b6f6b3b834251254cabd66f19c3e8
Tom Shea <tom@shea.at>
2013-10-28 23:29:45 +0000

    When the level loads, walls become aware of whether they have neighbors

--- scripts/level.js
+++ scripts/level.js
@@ -51,17 +51,66 @@ Level.prototype.fromStringArray = function(lines) {
     }
   }
 }
+Level.prototype.assign_blocking = function() {
+  for(var x = 0; x < this.width; x++) {
+    for(var y = 0; y < this.height; y++) {
+      var wall = this.walls[x][y];
+      if(!wall)
+        continue;
+      wall.left_blocked = false;
+      wall.right_blocked = false;
+      wall.top_blocked = false;
+      wall.bot_blocked = false;
+      wall.top_left_blocked = false;
+      wall.top_right_blocked = false;
+      wall.bot_left_blocked = false;
+      wall.bot_right_blocked = false;
+      if(x > 0) {
+        if(this.walls[x - 1][y])
+          wall.left_blocked = true;
+        if(y > 0 && this.walls[x - 1][y - 1])
+          wall.top_left_blocked = true;
+        if(y < this.height - 1 && this.walls[x - 1][y + 1])
+          wall.bot_left_blocked = true;
+      }
+      if(y > 0 && this.walls[x][y - 1])
+        wall.top_blocked = true;
+      if(x < this.width - 1) {
+        if(this.walls[x + 1][y])
+          wall.right_blocked = true;
+        if(y > 0 && this.walls[x + 1][y - 1])
+          wall.top_right_blocked = true;
+        if(y < this.height - 1 && this.walls[x + 1][y + 1])
+          wall.bot_right_blocked = true;
+      }
+      if(y < this.height - 1 && this.walls[x][y + 1])
+        wall.bot_blocked = true;
+
+      if(x == 0)
+        wall.left_blocked = true;
+      if(y == 0)
+        wall.top_blocked = true;
+      if(x == this.width)
+        wall.right_blocked = true;
+      if(y == this.height)
+        wall.bot_blocked = true;
+    }
+  }
+}
 var currentLevel;
 Level.load = function(name) {
   var lastLevel;
   if(currentLevel)
     lastLevel = currentLevel;
+
   require(name, 'level', function() {
     if(lastLevel) {
       lastLevel.destroy();
       delete(lastLevel);
     }
+    currentLevel.assign_blocking();
     registerRenderCleanup();
     triggerBackgroundRender();
   });
+
 }
